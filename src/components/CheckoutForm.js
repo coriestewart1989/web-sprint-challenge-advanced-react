93c135c59fe9050e7245dcd446dc461aa05b28ac
Corie Stewart <coriestewart1989>
2021-02-12 08:24:51 -0500

    completed

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import useForm from '../hooks/useForm'
 
 const initialValue = {
   firstName: "",
@@ -15,11 +16,9 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  // const [values, setValues] = useState(initialValue);
+const[{ firstName, lastName, address, city, state, zip }, handleChanges] = useForm(initialValue)
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -34,7 +33,7 @@ const CheckoutForm = (props) => {
           First Name:
           <input
             name="firstName"
-            value={values.firstName}
+            value={firstName}
             onChange={handleChanges}
           />
         </label>
@@ -42,7 +41,7 @@ const CheckoutForm = (props) => {
           Last Name:
           <input
             name="lastName"
-            value={values.lastName}
+            value={lastName}
             onChange={handleChanges}
           />
         </label>
@@ -50,21 +49,21 @@ const CheckoutForm = (props) => {
           Address:
           <input
             name="address"
-            value={values.address}
+            value={address}
             onChange={handleChanges}
           />
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input name="city" value={city} onChange={handleChanges} />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input name="state" value={state} onChange={handleChanges} />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input name="zip" value={zip} onChange={handleChanges} />
         </label>
         <button>Checkout</button>
       </form>
@@ -72,17 +71,17 @@ const CheckoutForm = (props) => {
       {showSuccessMessage && (
         <div className="success-message" data-testid="successMessage">
           <p>
-            You have ordered some plants! Woo-hoo! <span role="img">🎉</span>
+            You have ordered some plants! Woo-hoo! <span role="img"></span>
           </p>
           <p>Your new green friends will be shipped to:</p>
           <br />
           <br />
           <p>
-            {values.firstName} {values.lastName}
+            {firstName} {lastName}
           </p>
-          <p>{values.address}</p>
+          <p>{address}</p>
           <p>
-            {values.city}, {values.state} {values.zip}
+            {city}, {state} {zip}
           </p>
         </div>
       )}
--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,39 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/react'
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form needs to be completed in order to submit", () => {
+    render(<CheckoutForm />)
+
+    const firstName = screen.getByLabelText(/first name:/i);
+    const lastName = screen.getByLabelText(/last name:/i)
+    const address = screen.getByLabelText(/address:/i);
+    const city = screen.getByLabelText(/city:/i);
+    const state = screen.getByLabelText(/state:/i);
+    const zip = screen.getByLabelText(/zip:/i);
+    fireEvent.change(firstName,{target: {value:"Tia", name: "firstName"}})
+    fireEvent.change(lastName,{target: {value:"Marie", name: "lastName"}})
+    fireEvent.change(address,{target: {value:"123 fake st", name: "address"}})
+    fireEvent.change(city,{target: {value:"Provo", name: "city"}})
+    fireEvent.change(state,{target: {value:"Utah", name: "state"}})
+    fireEvent.change(zip,{target: {value:"84606", name: "zip"}})
+
+    expect(firstName).toHaveValue('Tia')
+    expect(lastName).toHaveValue('Marie')
+    expect(address).toHaveValue('123 fake st')
+    expect(city).toHaveValue('Provo')
+    expect(state).toHaveValue('Utah')
+    expect(zip).toHaveValue('84606')
+
+
+
+
+    
+});
